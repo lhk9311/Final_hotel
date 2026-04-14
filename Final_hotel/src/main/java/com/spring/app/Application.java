6a6312fb8677620c533c3d0ec4058b58eec5efed
lhk9311 <lhk9311@gmail.com>
2026-04-15 01:35:41 +0900

    jenkins test

--- Final_hotel/src/main/java/com/spring/app/Application.java
+++ Final_hotel/src/main/java/com/spring/app/Application.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
-@EnableScheduling   //  스프링 스케줄러
+@EnableScheduling   //  스프링 스케줄러 ss
 public class Application {
 
     public static void main(String[] args) {
